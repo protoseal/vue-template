68afbd0bee3bcc4ee27f5c7a0b5e8df5cd5f7a37
Vladimir Volkov <volkovvova67@gmail.com>
2025-07-30 17:00:01 +0400

    feat: add aliaces

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  resolve: {
+    alias: {
+      "@00_app": path.resolve(__dirname, "src/00_app"),
+      "@01_pages": path.resolve(__dirname, "src/01_pages"),
+      "@02_widgets": path.resolve(__dirname, "src/02_widgets"),
+      "@03_features": path.resolve(__dirname, "src/03_features"),
+      "@04_entities": path.resolve(__dirname, "src/04_entities"),
+      "@05_shared": path.resolve(__dirname, "src/05_shared"),
+    },
+  },
+});
