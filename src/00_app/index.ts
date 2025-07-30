5f2c8a674ae8383b6c82a2101805c6c883df90e4
Vladimir Volkov <volkovvova67@gmail.com>
2025-07-30 18:32:59 +0400

    feat: add pinia

--- src/00_app/index.ts
+++ src/00_app/index.ts
@@ -1,9 +1,11 @@
 import { createApp } from "vue"
 
-import { router } from "./plugins/router"
+import { pinia, router } from "./plugins"
 import App from "./ui/App.vue"
 
 const app = createApp(App)
 
 app.use(router)
+app.use(pinia)
+
 app.mount("#app")
